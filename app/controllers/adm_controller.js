a434b94c8a23471658696d125efb94e332e2e6aa
WillianGust <williandes.silva@yahoo.ca>
2023-05-16 19:20:48 -0400

    WIP: refatorando controller

--- app/controllers/adm_controller.js
+++ app/controllers/adm_controller.js
@@ -1,22 +1,65 @@
-var Adm = require('../models/adm');
+// var Adm = require('../models/adm');
 
 
-const AdmControler = {
-  index: (req, res, next) => {
-    Adm.find().then(dado => res.send(dado));
+// const AdmControler = {
+//   index: (req, res, next) => {
+//     Adm.find().then(dado => res.send(dado));
     
-    //     res.send(dado);
-    // const adm = new Adm({nome: 'Willian', senha: '123456', email: 'willian@gmail.com'});
-    // adm.save(error => {
-    //   if(error){
-    //   res.send(error);
-    //     return
-    //   }
-    //   Adm.find().then(dado => {
-    //     res.send(dado);
-    //   });
-    // });
-  }
-}
+//         res.send(dado);
+//     const adm = new Adm({nome: 'Willian', senha: '123456', email: 'willian@gmail.com'});
+//     adm.save(error => {
+//       if(error){
+//       res.send(error);
+//         return
+//       }
+//       Adm.find().then(dado => {
+//         res.send(dado);
+//       });
+//     });
+//   }
+// }
 
-module.exports = AdmControler;
+// module.exports = AdmControler;
+
+
+// GPT second
+
+// const Adm = require('../models/adm');
+
+// const AdmController = {
+//   index: async (req, res, next) => {
+//     try {
+//       const dados = await Adm.find();
+//       res.send(dados);
+
+//       const adm = new Adm({ nome: 'Willian', senha: '123456', email: 'williansilva@gmail.com' });
+//       await adm.save();
+
+//       const novosDados = await Adm.find();
+//       res.send(novosDados); // Removido o res.send(dados) duplicado
+//     } catch (error) {
+//       res.send(error);
+//     }
+//   }
+// };
+
+// module.exports = AdmController;
+
+// GPT Refatored
+const Adm = require('../models/adm');
+
+const AdmController = {
+  index: async (req, res, next) => {
+    try {
+      const adm = new Adm({ nome: 'Willian', senha: '123456', email: 'silva.willian@gmail.com' });
+      await adm.save();
+
+      const dados = await Adm.find();
+      res.send(dados);
+    } catch (error) {
+      res.send(error);
+    }
+  }
+};
+
+module.exports = AdmController;
